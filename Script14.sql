bb5242ea610905fc125f8b83252a4fcd19dd1a91
azuregithub-code <79245309+azuregithub-code@users.noreply.github.com>
2021-03-11 21:09:58 +0530

    Delete Script14.sql

--- Script14.sql
+++
@@ -1,6 +0,0 @@
--- Calculates the size of individual database objects.
-SELECT sys.objects.name, SUM(reserved_page_count) * 8.0 / 1024
-FROM sys.dm_db_partition_stats, sys.objects
-WHERE sys.dm_db_partition_stats.object_id = sys.objects.object_id
-GROUP BY sys.objects.name;
-GO
