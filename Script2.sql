f245f4d58d353ad8585cc5a289bd518173054b7d
azuregithub-code <79245309+azuregithub-code@users.noreply.github.com>
2021-03-11 21:12:17 +0530

    Delete Script2.sql

--- Script2.sql
+++
@@ -1,5 +0,0 @@
-SELECT TOP 10 req.session_id, req.start_time, cpu_time 'cpu_time_ms',
- OBJECT_NAME(ST.objectid, ST.dbid) 'ObjectName', SUBSTRING(REPLACE(REPLACE(SUBSTRING(ST.text, (req.statement_start_offset / 2)+1, ((CASE statement_end_offset WHEN -1 THEN DATALENGTH(ST.text)ELSE req.statement_end_offset END-req.statement_start_offset)/ 2)+1), CHAR(10), ' '), CHAR(13), ' '), 1, 512) AS statement_text FROM sys.dm_exec_requests AS req
-    CROSS APPLY sys.dm_exec_sql_text(req.sql_handle) AS ST
-ORDER BY cpu_time DESC;
-GO
