4107ca9326e7fda4de23934011a55d4c02b930f9
azuregithub-code <79245309+azuregithub-code@users.noreply.github.com>
2021-03-11 21:13:38 +0530

    Delete Script22.sql

--- Script22.sql
+++
@@ -1,9 +0,0 @@
-SELECT table_name = schema_name(o.schema_id) + '.' + o.name
-, wt.wait_duration_ms, wt.wait_type, wt.blocking_session_id, wt.resource_description
-, tm.resource_type, tm.request_status, tm.request_mode, tm.request_session_id
-FROM sys.dm_tran_locks AS tm
-INNER JOIN sys.dm_os_waiting_tasks as wt ON tm.lock_owner_address = wt.resource_address
-LEFT OUTER JOIN sys.partitions AS p on p.hobt_id = tm.resource_associated_entity_id
-LEFT OUTER JOIN sys.objects o on o.object_id = p.object_id or tm.resource_associated_entity_id = o.object_id
-WHERE resource_database_id = DB_ID()
-AND object_name(p.object_id) = '<table_name>';
