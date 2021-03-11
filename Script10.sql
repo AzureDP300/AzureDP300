bf0cc4c827d324be6857091eefeaf8fb365a755e
azuregithub-code <79245309+azuregithub-code@users.noreply.github.com>
2021-03-11 20:17:13 +0530

    Add files via upload

---
+++ Script10.sql
@@ -0,0 +1,8 @@
+SELECT wait_type,
+       SUM(wait_time) AS total_wait_time_ms
+FROM sys.dm_exec_requests AS req
+    JOIN sys.dm_exec_sessions AS sess
+        ON req.session_id = sess.session_id
+WHERE is_user_process = 1
+GROUP BY wait_type
+ORDER BY SUM(wait_time) DESC;
